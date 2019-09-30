19b0234ce7b92ab320ee0aec133c185cb0dd0dee
jimmylaskera <38056214+jimmylaskera@users.noreply.github.com>
2019-09-30 00:36:10 -0300

    Adicionado heapsort

---
+++ src/br/com/waldson/aula11/Main.java
@@ -0,0 +1,39 @@
+﻿package br.com.waldson.aula11;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        FilaBanco fila = new FilaBanco();
+
+        fila.insert("Fulano", 20);
+
+        fila.insert("Sicrano", 23);
+
+        fila.insert("Beltrano", 45);
+
+        fila.insert("Vovó", 65);
+
+        fila.insert("Tio", 48);
+
+        fila.insert(new Pessoa("Primo", 18));
+
+        fila.heapsort();
+
+
+
+        while (fila.getSize() > 0) {
+
+            Pessoa p = fila.peek();
+
+            System.out.println("Atendendo " + p.getNome());
+
+            fila.remove();
+
+        }
+
+    }
+
+}
